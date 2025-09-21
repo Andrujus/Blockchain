cbe9985015b053eafd54a701e230d9a9b86ec961
Andrujus <justasandrusk@gmail.com>
2025-09-21 21:34:11 +0300

    Lavinos efekto realizacija

--- ind_hash/hash_be_ai.py
+++ ind_hash/hash_be_ai.py
@@ -1,13 +1,31 @@
-
+def rotl64(x, r):
+    return ((x << r) & ((1 << 64) - 1))
 
 def hash_string():
     user_input = input("Iveskite teksta: ")
     MASK64 = (1 << 64) - 1
-    h = 0x1234567890abcdef
+    h1 = 0x1234567890abcdef
+    h2 = 0xfedcba0987654321
+    h3 = 0xabcdef1234567890
+    h4 = 0x0987654321fedcba
     user_input = user_input.encode('utf-8')
     for b in user_input:
-        h = (h*31 + b) & MASK64
-    print(f"Hash'as: {h}")
+        h1 ^= b
+        h1 = rotl64(h1, 13)
+        h1 = (h1 * 31 + b ^ (b >> 3)) & MASK64
+        h2 ^= rotl64(b, 7)
+        h2 = (h2 * 27 + b ^ (b >> 5)) & MASK64
+        h3 ^= rotl64(b, 10)
+        h3 = (h3 * 29 + b ^ (b >> 7)) & MASK64
+        h4 ^= rotl64(b, 17)
+        h4 = (h4 * 37 + b ^ (b >> 11)) & MASK64
+        
+    rez1 = f"{h1:016x}"
+    rez2 = f"{h2:016x}"
+    rez3 = f"{h3:016x}"
+    rez4 = f"{h4:016x}"
+
+    print(f"Hash'as: {rez1}{rez2}{rez3}{rez4}")
 
 
 
