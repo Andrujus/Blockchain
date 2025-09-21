84cfb9f22d84869788c1b374bd44f51e06f9faf5
Ignas Volodko <173953117+ignasvo@users.noreply.github.com>
2025-09-21 18:06:04 +0300

    baitu skaidymas po 4

--- ind_hash/hash_su_ai.py
+++ ind_hash/hash_su_ai.py
@@ -1,2 +1,15 @@
+import struct
+
 def rotl32(x, r):
     return ((x << r) & 0xFFFFFFFF) | (x >> (32 - r))
+
+print(hex(rotl32(0b1011, 1)))
+
+def u32_le(b, i):
+    # paimame 4 baitus nuo pozicijos i
+    chunk = b[i:i+4]
+    if len(chunk) < 4:
+        # jei trūksta baitų – pridedam nuliais
+        chunk = chunk + b'\x00' * (4 - len(chunk))
+    # paverčiam į 32 bitų skaičių (little-endian)
+    return struct.unpack('<I', chunk)[0]
