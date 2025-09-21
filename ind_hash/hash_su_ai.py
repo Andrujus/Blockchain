02e6e1196a17310a08cdc4df465e4fa9c876799e
Ignas Volodko <173953117+ignasvo@users.noreply.github.com>
2025-09-21 18:09:25 +0300

    padding

--- ind_hash/hash_su_ai.py
+++ ind_hash/hash_su_ai.py
@@ -13,3 +13,11 @@ def u32_le(b, i):
         chunk = chunk + b'\x00' * (4 - len(chunk))
     # paverčiam į 32 bitų skaičių (little-endian)
     return struct.unpack('<I', chunk)[0]
+
+def pad(data: bytes) -> bytes:
+    bit_len = (len(data) * 8) & 0xFFFFFFFF  # kiek bitų turėjo pradinis tekstas
+    padded = data + b'\x80'                 # pridedam 0x80
+    while len(padded) % 4 != 0:             # pildom nuliais, kol ilgis % 4 == 0
+        padded += b'\x00'
+    padded += struct.pack('<I', bit_len)    # gale pridedam 4 baitus su ilgiu
+    return padded
