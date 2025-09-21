96115b0439bb40c001884475e750f772f405314a
Ignas Volodko <173953117+ignasvo@users.noreply.github.com>
2025-09-21 18:11:31 +0300

    sumaisymas

--- ind_hash/hash_su_ai.py
+++ ind_hash/hash_su_ai.py
@@ -21,3 +21,13 @@ def pad(data: bytes) -> bytes:
         padded += b'\x00'
     padded += struct.pack('<I', bit_len)    # gale pridedam 4 baitus su ilgiu
     return padded
+
+def mix_once(st):
+    for i in range(8):
+        a = st[i]
+        b = st[(i+1) % 8]
+        # čia pagrindinis maišymas
+        c = ((a ^ rotl32(b, (i+1) % 31)) * 0x85ebca6b) & 0xFFFFFFFF
+        st[i] = ((st[i] + c) ^ rotl32(st[(i+3) % 8], (i*7) % 31)) & 0xFFFFFFFF
+    # dar pamaišom sukeisdami vietomis
+    st[0], st[2], st[4], st[6] = st[2], st[4], st[6], st[0]
