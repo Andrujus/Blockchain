baad949eef70b2e81c5c7f02bca0cd25a557a1bc
Andrujus <justasandrusk@gmail.com>
2025-12-10 12:56:18 +0200

    order

--- order.sol
+++ order.sol
@@ -2,40 +2,61 @@
 pragma solidity ^0.8.26;
 
 // This will only compile via IR
-contract Coin {
+contract Order {
     // The keyword "public" makes variables
     // accessible from other contracts
-    address public minter;
-    mapping(address => uint) public balances;
+    address public creator;
+    address public seller;
+    address public buyer;
+    uint public price;
+    bool public isCompleted;
+    string public description;
 
     // Events allow clients to react to specific
     // contract changes you declare
-    event Sent(address from, address to, uint amount);
+    event OrderCreated(address indexed creator, address indexed seller, uint price, string description);
+    event OrderCompleted(address indexed buyer, uint price);
 
     // Constructor code is only run when the contract
     // is created
-    constructor() {
-        minter = msg.sender;
+    constructor(address _seller, uint _price, string memory _description) {
+        creator = msg.sender;
+        seller = _seller;
+        price = _price;
+        description = _description;
+        isCompleted = false;
+        emit OrderCreated(creator, seller, price, description);
     }
 
-    // Sends an amount of newly created coins to an address
-    // Can only be called by the contract creator
-    function mint(address receiver, uint amount) public {
-        require(msg.sender == minter);
-        balances[receiver] += amount;
+    // Allows the seller to complete the order
+    // Can only be called by the seller
+    function completeOrder() public payable {
+        require(msg.sender == seller, "Only seller can complete the order");
+        require(!isCompleted, "Order is already completed");
+        require(msg.value == price, "Incorrect payment amount");
+
+        isCompleted = true;
+        buyer = msg.sender;
+        emit OrderCompleted(buyer, price);
     }
 
-    // Errors allow you to provide information about
-    // why an operation failed. They are returned
-    // to the caller of the function.
-    error InsufficientBalance(uint requested, uint available);
+    // Allows the buyer to retrieve the seller's address
+    function getSeller() public view returns (address) {
+        return seller;
+    }
 
-    // Sends an amount of existing coins
-    // from any caller to an address
-    function send(address receiver, uint amount) public {
-        require(amount <= balances[msg.sender], InsufficientBalance(amount, balances[msg.sender]));
-        balances[msg.sender] -= amount;
-        balances[receiver] += amount;
-        emit Sent(msg.sender, receiver, amount);
+    // Allows anyone to check if the order is completed
+    function checkOrderStatus() public view returns (bool) {
+        return isCompleted;
+    }
+
+    // Allows anyone to retrieve the order description
+    function getDescription() public view returns (string memory) {
+        return description;
+    }
+
+    // Allows anyone to retrieve the order price
+    function getPrice() public view returns (uint) {
+        return price;
     }
 }
